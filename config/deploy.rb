e6d244204be7708c77c158e6c7919a3d7bea6220
sleepinglion <admin@sleepinglion.pe.kr>
2016-11-23 15:21:59 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :default_env, {"RAILS_RELATIVE_URL_ROOT" => "/GeneAdmin"}
 set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, '.env','config/database.yml', 'config/secrets.yml'
+append :linked_files, '.env','config/database.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
