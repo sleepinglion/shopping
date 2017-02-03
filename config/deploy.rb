a5d952c401043fb5845eeb331f4dfcb92d7e8304
sleepinglion <admin@sleepinglion.pe.kr>
2017-02-03 14:43:54 +0900

    수정함:        Capfile 	수정함:        Gemfile.lock 	수정함:        config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,13 +35,26 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 # set :keep_releases, 5
 
 namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      within release_path do
+         execute :rake, 'tmp:clear'
+      end
     end
   end
 
-  after :finishing, 'deploy:restart'
+  desc 'Refresh sitemap'
+  task :refresh_sitemap do
+    on roles(:app), in: :sequence, wait: 1 do
+      within release_path do
+        with rails_env: (fetch(:rails_env) || fetch(:stage)) do
+          execute :rake, 'sitemap:refresh'
+        end
+      end
+    end
+  end
+
+#  after :finishing, 'deploy:refresh_sitemap'
+  after :finishing, 'deploy:cleanup'
 end
