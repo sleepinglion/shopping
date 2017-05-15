ebfc57676d41434e228bd77b7539ad442cd21ca5
sleepinglion <admin@sleepinglion.pe.kr>
2017-05-15 18:19:56 +0900

    새 파일:       db/migrate/20161105021483_create_product_categories.rb 	새 파일:       db/migrate/20161105021484_create_products.rb 	새 파일:       db/migrate/20161105021485_create_orders.rb

---
+++ db/migrate/20161105021484_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.references :product_category,:nulll=>false
+      t.string :title, :null=>false, :limit=>60
+      t.integer :price, :null=>false, :default => 0
+      t.integer :shipping_price, :null=>false, :default => 0
+      t.text :description, :null=>false
+      t.integer :product_pictures_count, :null => false, :default => 0
+      t.boolean :enable, :null=>false, :default=>false
+      t.timestamps
+    end
+
+    add_foreign_key :products, :product_categories, on_delete: :cascade, on_update: :cascade     
+  end
+end
---
+++ db/migrate/20161105021485_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.references :user
+      t.references :shipping,:nulll=>false
+      t.references :payment,:null=>false
+      t.integer :total_price, :null=>false, :default=>0
+      t.boolean :enable, :null=>false, :default=>false
+      t.timestamps
+    end
+    
+    add_foreign_key :orders, :users, on_delete: :cascade, on_update: :cascade 
+    add_foreign_key :orders, :shippings, on_delete: :cascade, on_update: :cascade 
+    add_foreign_key :orders, :payments, on_delete: :cascade, on_update: :cascade   
+  end
+end
