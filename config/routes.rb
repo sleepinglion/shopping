bc6940c6dcdbfaf8aaa02783d36c36c830349a76
sleepinglion <admin@sleepinglion.pe.kr>
2021-04-17 17:01:53 +0900

    수정

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks', :sessions => "users/sessions", :registrations => "users/registrations", :passwords => "users/passwords" }, :path_names => { :sign_up => 'new', :sign_in => 'login', :sign_out => 'logout' }
   as :user do
-    get 'new', :to => 'users::Registrations#new'
-    get 'edit', :to => 'users::Registrations#edit'
-    get 'login', :to => 'users::Sessions#new'
-    get 'logout', :to=> 'users::Sessions#destroy'
   end
 
   devise_for :admins, :controllers => { :sessions => "admins/sessions",:registrations => "admins/registrations" }, :path_names =>  {:sign_up=>'new',:sign_in => 'login', :sign_out => 'logout'} do
